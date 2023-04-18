802bc94b4ec10b2c6a404817931f0143de5b11fb
Lucia Richardson <lucilurichardson@gmail.com>
2023-04-18 11:55:46 -0300

    Main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,39 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AcercaDeComponent } from "./components/acerca-de/acerca-de.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { LogoAPComponent } from "./components/logo-ap/logo-ap.component";
+import { BannerComponent } from "./components/banner/banner.component";
+import { EducacionComponent } from "./components/educacion/educacion.component";
+import { ExperienciaComponent } from "./components/experiencia/experiencia.component";
+import { HysComponent } from "./components/hys/hys.component";
+import { ProyectoComponent } from "./components/proyecto/proyecto.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        AcercaDeComponent,
+        HeaderComponent,
+        LogoAPComponent,
+        BannerComponent,
+        EducacionComponent,
+        ExperienciaComponent,
+        HysComponent,
+        ProyectoComponent,
+        FooterComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        NgCircleProgressModule.forRoot({})
+    ]
 })
 export class AppModule { }
